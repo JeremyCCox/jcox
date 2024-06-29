cb3c138daba89f4d8b25720872a9b2f98b18db2e
JeremyCCox <jchristophercox@gmail.com>
2024-06-29 13:17:10 -0600

    Better configuration for GlitchyTitle

--- components/layout/GlitchyTitle.tsx
+++ components/layout/GlitchyTitle.tsx
@@ -3,66 +3,234 @@
 import {useCallback, useEffect, useState} from "react";
 import {random} from "nanoid";
 
+interface ScrambleTitle{
+    title: string;
+    selections: string[];
+    rare:string[];
+    style?:React.CSSProperties;
+}
+
 const possibilities1 = ["Software","Web","Database","Solutions","Computer"]
 const possibilities2 = ["Developer","Designer","Consulting","Professional","Hosting","Wizard"]
+const possibleTitle1:ScrambleTitle[] = [
+    {
+        title:"Software",
+        selections:[
+            "Developer",
+            "Designer",
+            "Consulting",
+            "Professional",
+        ],
+        rare:[
+            "Wizard",
+        ]
+
+    },
+    {
+        title:"Web",
+        selections:[
+            "Developer",
+            "Designer",
+            "Consulting",
+            "Professional",
+            "Hosting"
+        ],
+        rare:[
+            "Wizard",
+        ]
+
+    },
+    {
+        title:"Database",
+        selections:[
+            "Developer",
+            "Designer",
+            "Consulting",
+            "Hosting",
+        ],
+        rare:[
+            "Wizard",
+        ]
+
+    },
+    {
+        title:"Solutions",
+        selections:[
+            "Developer",
+            "Designer",
+            "Consulting",
+            "Professional",
+            "Hosting",
+        ],
+        rare:[
+            "Wizard",
+        ]
+
+    },
+    {
+        title:"Computer",
+        selections:[
+            "Designer",
+            "Consulting",
+            "Professional",
+        ],
+        rare:[
+            "Wizard",
+        ]
+
+    },
+]
+const possibleTitle2:ScrambleTitle[] = [
+    {
+        title:"Developer",
+        selections:[
+            "Software",
+            "Web",
+            "Database",
+            "Solutions",
+        ],
+        rare:[]
+
+    },
+    {
+        title:"Designer",
+        selections:[
+            "Software",
+            "Web",
+            "Database",
+            "Solutions",
+            "Computer"
+        ],
+        rare:[]
+
+    },
+    {
+        title:"Consulting",
+        selections:[
+            "Software",
+            "Web",
+            "Database",
+            "Solutions",
+            "Computer",
+        ],
+        rare:[]
+    },
+    {
+        title:"Professional",
+        selections:[
+            "Software",
+            "Web",
+            "Solutions",
+            "Computer",
+        ],
+        rare:[]
+
+    },
+    {
+        title:"Hosting",
+        selections:[
+            "Web",
+            "Database",
+            "Solutions",
+        ],
+        rare:[]
+
+    },
+    {
+        title:"Wizard",
+        selections: [
+            "Software",
+            "Web",
+            "Database",
+            "Solutions",
+            "Computer"
+        ],
+        rare:[
+        ],
+        style:{
+            color:"lightcyan",
+            animationDuration:'1s',
+            animationIterationCount:999,
+            animationName:'shakeTitle',
+        }
+    }
+]
 
 export default function GlitchyTitle(){
 
-    const [title1, setTitle1]=useState(possibilities1[0])
-    const [title2, setTitle2]=useState(possibilities2[0])
+    const [title1, setTitle1]=useState(possibleTitle1[0])
+    const [title2, setTitle2]=useState(possibleTitle2[0])
 
     useEffect(()=>{
         let intervalTime = parseInt((Math.random()*10000).toFixed(0))+4000
        const interval = setInterval(()=>{
-           if(["Developer","Hosting"].includes(title2)){
-               let safeVal = possibilities1[Math.floor(Math.random()*possibilities1.length)]
-               while (safeVal == "Computer"){
-                   console.log("Case caught")
-                   safeVal = possibilities1[Math.floor(Math.random()*possibilities1.length)]
-               }
-               setTitle1(safeVal)
+           // let possible: ScrambleTitle = possibleTitle2.find(({title})=>title===title2) || possibleTitle2[0];
+           let randVal = Math.random();
+           if (randVal > .95){
+               let selections = title2.selections.concat(title2.rare)
+               let selected:ScrambleTitle = possibleTitle1.find(({title})=>title===selections[Math.floor(Math.random()*selections.length)])||possibleTitle1[0]
+               setTitle1(selected)
            }else{
-               setTitle1(possibilities1[Math.floor(Math.random()*possibilities1.length)])
+               setTitle1(possibleTitle1.find(({title})=>title===title2.selections[Math.floor(Math.random()*title2.selections.length)])||possibleTitle1[0])
            }
+
+           // if(["Developer","Hosting"].includes(title2)){
+           //     let safeVal = possibilities1[Math.floor(Math.random()*possibilities1.length)]
+           //     while (safeVal == "Computer"){
+           //         console.log("Case caught")
+           //         safeVal = possibilities1[Math.floor(Math.random()*possibilities1.length)]
+           //     }
+           //     setTitle1(safeVal)
+           // }else{
+           //     setTitle1(possibilities1[Math.floor(Math.random()*possibilities1.length)])
+           // }
         },intervalTime)
         return(()=>{
             clearInterval(interval)
         })
-    },[title1])
+    },[title1, title2.rare, title2.selections])
     useEffect(()=>{
         let intervalTime = parseInt((Math.random()*10000).toFixed(0))+4000
         const interval = setInterval(()=>{
-
-            if(title1==="Computer"){
-                let safeVal = possibilities2[Math.floor(Math.random()*possibilities2.length)]
-                while (["Developer","Hosting"].includes(safeVal)){
-                    console.log("Case caught")
-                    safeVal = possibilities2[Math.floor(Math.random()*possibilities2.length)]
-                }
-                setTitle2(safeVal)
+            // let possible: ScrambleTitle = possibleTitle1.find(({title})=>title===title1) || possibleTitle1[0];
+            let randVal = Math.random();
+            if (randVal > .95){
+                let selections = title1.selections.concat(title1.rare)
+                let selected:ScrambleTitle = possibleTitle2.find(({title})=>title===selections[Math.floor(Math.random()*selections.length)])||possibleTitle2[0]
+                setTitle2(selected)
             }else{
-                setTitle2(possibilities2[Math.floor(Math.random()*possibilities2.length)])
+                setTitle2(possibleTitle2.find(({title})=>title===title1.selections[Math.floor(Math.random()*title2.selections.length)])||possibleTitle2[0])
             }
+            // if(title1==="Computer"){
+            //     let safeVal = possibilities2[Math.floor(Math.random()*possibilities2.length)]
+            //     while (["Developer","Hosting"].includes(safeVal)){
+            //         console.log("Case caught")
+            //         safeVal = possibilities2[Math.floor(Math.random()*possibilities2.length)]
+            //     }
+            //     setTitle2(safeVal)
+            // }else{
+            //     setTitle2(possibilities2[Math.floor(Math.random()*possibilities2.length)])
+            // }
         },intervalTime)
         return(()=>{
             clearInterval(interval)
         })
-    },[title2])
+    },[title1.rare, title1.selections, title2])
     return(
        <>
 
            <div className={'flex relative'}>
-               {possibilities1.map((title,index)=>{
+               {possibleTitle1.map((title,index)=>{
                    return(
                        <h2 className={title===title1?'px-8 top-0 transition-all ease-in duration-500':'absolute px-8 translate-y-[1lh] opacity-0 transition-all ease-in duration-500'} key={"title1"+index}>
-                           {`${title}`}
+                           {`${title.title}`}
                        </h2>
                    )
                })}
-               {possibilities2.map((title,index)=>{
+               {possibleTitle2.map((title,index)=>{
                    return(
-                       <h2 className={title===title2?'absolute translate-x-[200px] transition-all ease-in duration-500':'absolute translate-x-[200px] translate-y-[1lh] opacity-0 transition-all ease-in duration-500'} key={"title1"+index}>
-                           {`${title}`}
+                       <h2 style={title.style} className={title===title2?'absolute translate-x-[200px] transition-all ease-in duration-500':'absolute translate-x-[200px] translate-y-[1lh] opacity-0 transition-all ease-in duration-500'} key={"title1"+index}>
+                           {`${title.title}`}
                        </h2>
                    )
                })}
