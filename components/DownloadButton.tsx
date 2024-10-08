91f265aaf398ceb29812683174cf546d503a4f8b
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-10-08 15:30:21 -0600

    Release Select & Download Button

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -1,37 +1,48 @@
 'use client'
 import { request } from "@octokit/request";
-import { useEffect, useState } from "react";
+import React, {ChangeEvent, useEffect, useState } from "react";
+import ReleaseSelect from "./ReleaseSelect";
+
+export interface ReleasesType{
+    [key:string]: Record<string,any>
+}
+interface ReleaseType{
+    [key:string]:any,
+}
 
 export default function DownloadButton(){
 
-    const [releaseSource, setReleaseSource] = useState({url:"",name:""})
-    const [osType, setOsType] = useState("Unknown")
-
+    const [releaseSource, setReleaseSource] = useState({} as ReleaseType)
+    const [releases,setReleases] = useState({}as ReleasesType)
+    const [osType, setOsType] = useState("Default")
     useEffect(() => {
-        let userAgent = navigator.userAgent;
-        switch(true){
-            case(userAgent.includes("Windows")):
-                setOsType("Windows")
-                break;
-            case(userAgent.includes("Linux")):
-                setOsType("Linux")
-                break;
-            case(userAgent.includes("X11")):
-                setOsType("Unix")
-                break;
-            case(userAgent.includes("Mac")):
-                setOsType("Mac Os")
-                break;
-            default:
-                setOsType("Unknown")
-                break;
-        }
+        checkOsType()
+        getReleases()
     }, [])
 
-    useEffect(() => {
-        getReleases();
-    }, [osType]);
-
+    const checkOsType=()=> {
+        let userAgent = navigator.userAgent;
+        let osType;
+        switch (true) {
+            case(userAgent.includes("Windows")):
+                osType = "Windows"
+                break;
+            case(userAgent.includes("Linux")):
+                osType = "Linux"
+                break;
+            case(userAgent.includes("X11")):
+                osType = "Unix"
+                break;
+            case(userAgent.includes("Mac")):
+                osType = "MacOs"
+                break;
+            default:
+                osType = "Default"
+                break;
+        }
+        setOsType(osType)
+        return osType
+    }
     const getReleases = async () => {
         const queryParams = new URLSearchParams()
         queryParams.set("per_page", "5")
@@ -43,48 +54,86 @@ export default function DownloadButton(){
                 authorization: process.env["NEXT_PUBLIC_GIT_RELEASES_READ_TOKEN"]
             }
         })
-        switch (osType){
-            case("Windows"):
-                let asset = res.data[0].assets.find(({name})=>name.split(".").pop() ==="msi");
-                if(!asset){
-                    console.error("Could not find release for OS type!")
-                    return
+        let releases:ReleasesType = {} as ReleasesType;
+        res.data.map(release=>{
+            let releaseBuilds:ReleaseType= {}
+            release.assets.map(asset=>{
+                let name = asset.name.split(".").pop()
+                switch (name){
+                    case("msi"):
+                        // releaseBuilds["Windows"]= {...releaseBuilds["Windows"],[name]:asset}
+                        releaseBuilds["Windows"] = asset
+                        break;
+                    case("dmg"):
+                        releaseBuilds["MacOs"] = asset
+                        break;
+                    case("gz"):
+                        releaseBuilds["Linux"] = asset
+                        break;
+                    case("deb"):
+                        releaseBuilds["Debian"] = asset
+                        break;
+                    case("rpm"):
+                        releaseBuilds["Redhat/CentOs"] = asset
+                        break;
+                    case("exe"):
+                        releaseBuilds["Default"] = asset
+                    // case("msi"):
+                    //     break;
                 }
-                setReleaseSource({url:asset.browser_download_url,name:asset.name})
-                break
-            case("Linux"):
-                break
-            default:
-                console.log('default')
-        }
+            })
+            if(!releaseBuilds["Windows"] && releaseBuilds["Default"]){
+                releaseBuilds["Windows"] = releaseBuilds["Default"]
+            }
+            releases[release.name || crypto.randomUUID()] = releaseBuilds
+        })
 
+        setReleases(releases)
+        setReleaseSource(Object.values(releases)[0])
+    }
+    const changeRelease=(e:ChangeEvent<HTMLSelectElement>)=>{
+        let rs = releases[e.currentTarget.value]
+        checkOsType()
+        setReleaseSource(rs)
+    }
+    const changeOs=(e:ChangeEvent<HTMLSelectElement>)=>{
+        setOsType(e.currentTarget.value)
     }
     const download=async ()=>{
         let a = document.createElement("a")
-        a.href = releaseSource.url;
+        a.href = releaseSource[osType]["browser_download_url"]
         a.click()
         a.remove()
     }
     return(
-        <div className={'text-center'}>
-            <button className={'border-white border-2 rounded-md text-4xl px-16 py-6'} onClick={download} type={"button"}>
+        <div className={'text-center grid max-h-[500px] grow'}>
+            <label className={'text-amber-100'}>
+                Version:
+                <span className={'border '}>
+                    <ReleaseSelect releases={releases} handleChange={changeRelease}/>
+                </span>
+            </label>
+            <button className={'border-white border-2 rounded-md text-4xl px-16 py-6 h-fit hover:bg-gray-800 hover:cursor-pointer '} onClick={download} type={"button"}>
                 Download
             </button>
-            <h4 className={'text-sm'}>
+            <h4 className={'text-sm my-2'}>
                 {
-                    osType==="Unknown"?
+                    osType===undefined?
                         <>
+                            <ReleaseSelect releases={releaseSource} selected={osType} handleChange={changeOs}/>
                             Please Select a version to install!
                         </>
                         :
                         <>
-                            <span className={'text-lime-500 text-sm'}>{osType}</span> detected, recommended installer:
+                            <span className={'text-lime-500 text-lg hover:cursor-pointer'}>
+                                <ReleaseSelect releases={releaseSource} selected={osType} handleChange={changeOs}/>
+                            </span> installer:
+                            <p>
+                                {releaseSource[osType]?.name}
+                            </p>
                         </>
                 }
             </h4>
-            <p>
-                {releaseSource.name}
-            </p>
         </div>
     )
 }
