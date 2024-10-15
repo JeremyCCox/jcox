d3bb32f007ca575e5f1b420f662591eec2282389
JeremyCCox <jchristophercox@gmail.com>
2024-10-15 14:05:08 -0600

    Adding middleware for basic (VERY BASIC AND BUGGY) auth on /admin/* paths

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse,NextRequest } from 'next/server';
+import {setJwt, getJwtData, getJwt} from "test-jman-web-tokens";
+
+export async function middleware(req: NextRequest) {
+    try{
+        if(req.nextUrl.pathname === "/admin"){
+            return NextResponse.next()
+        }
+        let cookie = req.cookies.get("jwtoken")
+        if(cookie){
+            return NextResponse.next()
+        }
+        return NextResponse.redirect(new URL('/admin', req.url))
+    }catch(err){
+        return NextResponse.redirect(new URL('/admin',req.url))
+    }
+}
+
+// Define paths where the middleware should be applied
+export const config = {
+    matcher: [
+        '/admin/:path*',
+    ],
+};
