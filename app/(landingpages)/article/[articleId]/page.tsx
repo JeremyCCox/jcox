b870265e1c2b5b01f1fd51430fbab517c1a4bd49
JeremyCCox <jchristophercox@gmail.com>
2024-10-15 12:38:00 -0600

    Article Page has param type & set goBack button display to false

--- app/(landingpages)/article/[articleId]/page.tsx
+++ app/(landingpages)/article/[articleId]/page.tsx
@@ -3,12 +3,12 @@ import {ArticleType, getArticle} from "@/app/lib/ArticleServices";
 import {Suspense} from "react";
 import Article from "@/components/articles/Article";
 
-export default async function Page({params}){
-    const article:ArticleType = JSON.parse(await getArticle(params.articleId))
+export default async function Page({params}:Readonly<{params:{articleId:string}}>){
+    const article:ArticleType = await JSON.parse(await getArticle(params.articleId))
 
     return(
         <Suspense fallback={<>loading</>}>
-            <Article article={article} goBack={true}/>
+            <Article article={article} goBack={false}/>
         </Suspense>
     )
 }
