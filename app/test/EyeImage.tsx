0a359cb80a99256ba1e310b76110c43bebcf22d4
JeremyCCox <jchristophercox@gmail.com>
2025-09-12 07:38:24 +0000

    feat: add EyeImage component to contain the eyes in the image.

---
+++ app/test/EyeImage.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import Eyeball from "@/test/comps/Eyeball";
+import LeftIris from "@public/eyes/LeftIris.svg";
+import LeftEyeball from "@public/eyes/LeftEyeball.svg";
+import RightEyeball from "@public/eyes/RightEyeball.svg";
+import {useEffect, useRef} from 'react'
+
+export default function EyeImage(){
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+
+        /**
+         *  on window resize, (Debounced by a few seconds?)
+         *
+         *  let:
+         *      - base image width (bw)
+         *      - base image height (bh)
+         *
+         *  get:
+         *      - Image width (iw)
+         *      - Image height (ih)
+         *      - Desired y (dy)
+         *      - Desired x (dx)
+         *      - Desired eye width (ew)
+         *      - Desired eye height (eh)
+         *
+         *  then:
+         *      - Scale width ( sw ) = ( iw / bw )
+         *      - Scale height ( sh ) = ( ih / bh )
+         *
+         *      Scale the window:
+         *      h = sh * bh
+         *      w = sw * bw
+         *
+         *      Scale the eye
+         *      h = sh * eh
+         *      w = sw * ew
+         *
+         *      Set the new eye position:
+         *      ( x, y ) = ( dx + sw ( bw - dx ), dy + sh ( bh - dy ) )  // Courtesy of [Isaac from stack overflow](https://math.stackexchange.com/questions/109122/how-do-i-calculate-the-new-x-y-coordinates-and-width-height-of-a-re-sized-group)
+         *
+         *
+         */
+
+
+
+    }, []);
+
+    return (
+        <div className={'relative w-full h-full bg-red-300 flex'}>
+            <Image
+                src={'/JeremySmiling.jpg'}
+                alt={'Jeremy Smiling'}
+                style={{objectFit:"contain"}}
+                className="object-cover"
+                priority layout={'fill'}
+                ref={imageRef}
+            />
+            <Eyeball
+                irisSvg={LeftIris}
+                irisId={'leftIris'}
+                ballSvg={LeftEyeball}
+                ballId={'leftBall'}
+                className={'w-16 h-16 '}
+            />
+            <Eyeball
+                irisSvg={LeftIris} // Double left iris looks better.
+                irisId={'rightIris'}
+                ballSvg={RightEyeball}
+                ballId={'rightBall'}
+                className={'w-16 h-16'}
+            />
+        </div>
+    )
+}
