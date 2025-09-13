dab0505e96744e0711238aa04943a35d248503da
JeremyCCox <jchristophercox@gmail.com>
2025-09-13 01:00:11 +0000

    chore: remove unused imports

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,4 @@
 'use client'
-
-import RightEyeball from "@public/eyes/RightEyeball.svg"
-import LeftEyeball from "@public/eyes/LeftEyeball.svg"
-import LeftIris from "@public/eyes/LeftIris.svg"
-import Eyeball from "@/test/comps/Eyeball";
-import Image from "next/image"
 import EyeImage from "@/test/EyeImage";
 
 export default function Page(){
